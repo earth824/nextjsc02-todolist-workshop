ee7d2fc8dac324056e244599b3f8289ae796434a
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2025-09-14 18:01:45 +0700

    add generate metadata

--- app/(main)/todo/[todoId]/edit/page.tsx
+++ app/(main)/todo/[todoId]/edit/page.tsx
@@ -1,10 +1,24 @@
 import EditTodoForm from '@/components/edit-todo-form';
 import { fecthTodoById } from '@/libs/data';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
+// export const metadata: Metadata = {
+//   title: 'Edit'
+// };
 type EditTodoPageProps = {
   params: Promise<{ todoId: string }>;
 };
+
+export async function generateMetadata({
+  params
+}: EditTodoPageProps): Promise<Metadata> {
+  const { todoId } = await params;
+  const todo = await fecthTodoById(todoId);
+  return {
+    title: `Edit: ${todo?.title}`
+  };
+}
 
 export default async function EditTodoPage({ params }: EditTodoPageProps) {
   const { todoId } = await params;
